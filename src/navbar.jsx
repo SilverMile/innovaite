6ec3148e920567ac12a58cfcd7785f813fcc8ed0
yIsabellap <YouxiIsabella10052@dubaicollege.org>
2026-01-09 19:22:01 +0400

    Make Navbar and delete template

---
+++ src/navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './navbar.css';
+import userIcon from './assets/user-icon.svg';
+
+
+const Navvybar = () => {
+  return (
+    <nav className="Navvybar">
+      <div className="navbar-left">
+        <a href="/" className="logo">
+          Dihvik Dihtwani
+        </a>
+      </div>
+      <div className="navbar-center">
+        <ul className="nav-links">
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/map">Map</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <a href="/points" className="user-points">
+          <span className="points">0</span>
+        </a>
+        <a href="/account" className="user-icon">
+  <img src={userIcon} alt="User Icon" />
+</a>
+
+      </div>
+    </nav>
+  );
+};
+
+export default Navvybar;
